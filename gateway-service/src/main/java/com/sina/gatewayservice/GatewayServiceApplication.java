27d8c93ca47e3ba0c72e68d80f9bc33116f31916
Reza Sohbati <Sohbati@gmail.com>
2024-03-26 22:50:20 +0330

    prepare for gateway

--- gateway-service/src/main/java/com/sina/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/sina/gatewayservice/GatewayServiceApplication.java
@@ -3,8 +3,10 @@ package com.sina.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
--- gateway-service/src/main/java/com/sina/gatewayservice/HystrixController.java
+++ gateway-service/src/main/java/com/sina/gatewayservice/HystrixController.java
@@ -1,4 +1,4 @@
-package com.devglan.gatewayservice;
+package com.sina.gatewayservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
