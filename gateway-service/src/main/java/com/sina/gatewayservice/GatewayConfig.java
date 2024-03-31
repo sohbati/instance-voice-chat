2d9fbbcc4a77c656d372927bb91135a9d1b2375a
Reza Sohbati <Sohbati@gmail.com>
2024-04-01 02:08:33 +0330

    gateway configuring

---
+++ gateway-service/src/main/java/com/sina/gatewayservice/GatewayConfig.java
@@ -0,0 +1,21 @@
+package com.sina.gatewayservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("voice_chat_route", r -> r
+                        .path("/ws/voice-chat-signaling/**")
+                        .filters(f -> f.rewritePath("/ws/voice-chat-signaling/(?<remaining>.*)", "/${remaining}"))
+                        .uri("http://localhost:8082")
+                )
+                .build();
+    }
+}
